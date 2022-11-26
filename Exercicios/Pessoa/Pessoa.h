31c7bd552a99035bc2dc7076703748afc404c701
Carolina Nóbrega <notcarolinna@Gmail.com>
2022-11-26 01:40:59 -0300

    Create Pessoa.h

---
+++ Exercicios/Pessoa/Pessoa.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <iostream>
+#ifndef PESSOA_HP
+
+using namespace std;
+
+class Pessoa
+{
+private:
+	string Nome;
+	int Idade;
+	float Altura;
+	char Genero;
+
+public:
+	Pessoa();
+	Pessoa(string nome, int idade, float altura, char genero);
+	~Pessoa();
+
+	string getNome();
+	int getIdade();
+	float getAltura();
+	char getGenero();
+
+	void setNome(string nome);
+	void setIdade(int idade);
+	void setAltura(float altura);
+	void setGenero(char genero);
+
+	void ImprimeDados();
+
+};
+
+#endif
