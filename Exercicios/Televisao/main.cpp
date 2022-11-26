40e468595318e20f1e7a7f24948b47483fa72dd3
Carolina Nóbrega <notcarolinna@Gmail.com>
2022-11-26 14:13:52 -0300

    Update main.cpp

--- Exercicios/Televisao/main.cpp
+++ Exercicios/Televisao/main.cpp
@@ -1,5 +1,16 @@
+#include <iostream>
+#include "Televisao.h"
+using namespace std;
+
 // Implemente uma televisão. A televisão tem um controle de volume do som e um controle de seleção de canal.
 // O controle de volume permite aumentar ou diminuir a potência do volume de som em uma unidade de cada vez.
 // O controle de canal também permite aumentar e diminuir o número do canal em uma unidade, porém, também possibilita trocar para um canal indicado.
 // Também devem existir métodos para consultar o valor do volume de som e o canal selecionado.
 // No programa principal, crie uma televisão e troque de canal algumas vezes. Aumente um pouco o volume, e exiba o valor de ambos os atributos.
+
+int main() {
+
+	Televisao tv;
+	//Televisao* televisao = new Televisao();
+	tv.Dados();
+}
